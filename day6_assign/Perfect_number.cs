692c2444537cd68b09d932873f0a39d4b97221e0
parthgarg977 <parthgarg977@gmail.com>
2022-08-08 20:19:04 +0530

    UC2 1st commit

---
+++ day6_assign/Perfect_number.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace Day6_assign
+{
+    internal class Perfect_number
+    {
+        public void Pn1()
+        {
+            Console.WriteLine("Enter number");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            var factors = new ArrayList();
+            int j = 0;
+            for (int i =1; i < n1; i++)
+            {
+                if (n1 % i == 0)
+                {
+                    j = j + i;
+                }
+            }
+            if (j == n1)
+            {
+                Console.WriteLine("perfect number");
+            }
+            else
+            {
+                Console.WriteLine("not a perfect number ");
+            }
+           
+        }
+    }
+}
--- day6_assign/Program.cs
+++ day6_assign/Program.cs
@@ -7,6 +7,9 @@
             System.Console.WriteLine("Hello World!");
             Fibbo_series f1 = new Fibbo_series();
             f1.series();
+            Perfect_number pn = new Perfect_number();
+            pn.Pn1();
+
         }
     }
 }
